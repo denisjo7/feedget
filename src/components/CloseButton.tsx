92e3f7f66928938c7a8f8a2dc47435af99ee41b7
Denis Jonathan <denisjonathan7@gmail.com>
2022-05-05 00:01:21 -0300

    refactor: change scale X icon

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -7,7 +7,7 @@ export function CloseButton() {
       className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100"
       title="Fechar formulário de feedback"
     >
-      <X weight="bold" className="w-4 h-4" />
+      <X weight="bold" className="w-5 h-5" />
     </Popover.Button>
   );
 }
