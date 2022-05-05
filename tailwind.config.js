8c9cccb4f5deb09ed39e96d190a4ef90514035f4
Denis Jonathan <denisjonathan7@gmail.com>
2022-05-05 00:24:37 -0300

    chore: create custom border radius to 4px

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      borderRadius: {
+        md: '4px',
+      },
       colors: {
         brand: {
           300: '#996DFF',
