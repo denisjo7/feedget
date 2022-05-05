066490edc10cc0cb9635f9bc24fed1c78d232982
Denis Jonathan <denisjonathan7@gmail.com>
2022-05-04 23:28:35 -0300

    feat: create WidgetForm component

---
+++ src/components/WidgetForm.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { CloseButton } from './CloseButton';
+
+import bugImageUrl from '../assets/bug.svg';
+import ideaImageUrl from '../assets/idea.svg';
+import thoughtImageUrl from '../assets/thought.svg';
+
+const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Desenho de uma larvinha roxa',
+    },
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Desenho de uma lâmpada acesa',
+    },
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Desenho de uma nuvem de pensamento',
+    },
+  },
+};
+
+type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none "
+              key={key}
+              onClick={() => setFeedbackType(key as FeedbackType)}
+              type="button"
+            >
+              <img
+                className="px-0"
+                src={value.image.source}
+                alt={value.image.alt}
+              />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
+
+      <footer>
+        Feito com ♥ por{' '}
+        <a
+          className="underline underline-offset-1"
+          href="https://github.com/denisjo7"
+          target="_blank"
+        >
+          Denis Jonathan
+        </a>
+      </footer>
+    </div>
+  );
+}
