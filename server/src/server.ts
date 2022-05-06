07e5b893141a44868176d98df445dbec72852951
Denis Jonathan <denisjonathan7@gmail.com>
2022-05-06 03:44:16 -0300

    feat: implements sending emails with feedback content

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '15eab1d5e18c06',
+    pass: 'e0632bcd39e5ad',
+  },
+});
 
 app.post('/feedbacks', async (req: Request, res: Response) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req: Request, res: Response) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <freezing@feedget.com>',
+    to: 'Denis Jonathan <denisjonathan7@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Conteúdo do feedback: ${comment}</p>`,
+      `</div>`,
+    ].join('\n'),
   });
 
   return res.status(201).json({ data: feedback });
