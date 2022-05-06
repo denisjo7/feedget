b078d40471a004e1972e48fead55cd30219719d5
Denis Jonathan <denisjonathan7@gmail.com>
2022-05-06 03:34:33 -0300

    feat: create route to create feedback

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { prisma } from './prisma';
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/feedbacks', async (req: Request, res: Response) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return res.status(201).json({ data: feedback });
+});
 
 app.listen(3333, () => {
   console.log('✔️  Server running on port 3333!');
